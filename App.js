dd2a2286b64d4bf23c8252cb5abad14728458d72
xiaoer <1364335360@qq.com>
2020-05-22 15:35:07 +0800

    fitst

---
+++ App.js
@@ -0,0 +1,24 @@
+// import React,{ Component } from 'react';
+// import {Test,View} from 'react-natival';
+//
+// export default class App extends Component{
+//     render(){
+//         return(
+//             <View>
+//                 <Test>hello</Test>
+//             </View>
+//         )
+//     }
+// }
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> hello </Text>
+      </View>
+    );
+  }
+}
